1b2e4cb404020a105281ea03d32b1ef59d122d69
Jack Mallers <jimmymowschess@gmail.com>
2017-12-29 14:18:37 -0600

    feature(syncing): hard code mock the new style

--- app/components/LndSyncing/LndSyncing.js
+++ app/components/LndSyncing/LndSyncing.js
@@ -1,66 +1,60 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { FaCopy } from 'react-icons/lib/fa'
 import styles from './LndSyncing.scss'
 
-class LndSyncing extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      facts: [
-        {
-          title: 'The Lightning Network',
-          description: 'The Lightning Network is a second layer solution built on top of the Bitcoin block chain that attempts to increase Bitcoin\'s scalability and privacy' // eslint-disable-line
-        },
-        {
-          title: 'Payment Channel',
-          description: 'A payment channel is a class of techniques designed to allow users to make multiple Bitcoin transactions without commiting all of the transactions to the Bitcoin block chain. You can think of payment channels like tubes of money' // eslint-disable-line
-        },
-        {
-          title: 'HTLC',
-          description: 'Hashed TimeLock Contracts is a class of payments that use hashlocks and timelocks to require the receiver of a payment either acknowledge receiving the payment before a deadline or forfeit the ability to claim the payment. HTLCs are useful within the Lightning Network for routing payments across two or more payment channels' // eslint-disable-line
-        },
-        {
-          title: 'Onion Routing',
-          description: 'Onion routing is a technique for anonymous communication over a computer network. In an onion network, messages are encapsulated in layers of encryption, analogous to layers of an onion.' // eslint-disable-line
-        }
-      ],
-      currentFact: 0
-    }
-  }
 
+class LndSyncing extends Component {
   componentWillMount() {
     this.props.fetchBlockHeight()
   }
 
   render() {
     const { fetchingBlockHeight, syncPercentage } = this.props
-    const { facts, currentFact } = this.state
-    const renderCurrentFact = facts[currentFact]
 
     return (
       <div className={styles.container}>
-        <h3>zap</h3>
-        <div className={styles.loading}>
-          {!fetchingBlockHeight && <h4>{syncPercentage > 0 && `${syncPercentage}%`}</h4>}
-          <div className={styles.spinner} />
-          <h1>syncing your lightning node to the blockchain</h1>
-        </div>
-        <div className={styles.facts}>
-          <div className={styles.fact}>
-            <h2>{renderCurrentFact.title}</h2>
-            <p>{renderCurrentFact.description}</p>
+        <header>
+          <section>
+            <h3>zap</h3>
+          </section>
+          <section className={styles.loading}>
+            <h4>50%</h4>
+            <div className={styles.spinner} />
+          </section>
+        </header>
+        
+        <div className={styles.aliasForm}>
+          <h1>What should we call you?</h1>
+          <p>Give yourself a username to help others connect with you on the Lightning Network</p>
+
+          <div className={styles.inputContainer}>
+            <input
+              type='text'
+              placeholder='bcash bcash bcash bcash'
+              className={styles.input}
+              autoFocus
+            />
+
+            <div className={styles.submit}>
+              Submit
+            </div>
           </div>
-          <ul>
-            {
-              facts.map((fact, index) => (
-                <li
-                  className={`${styles.factButton} ${currentFact === index && styles.active}`}
-                  key={index}
-                  onClick={() => this.setState({ currentFact: index })}
-                />
-              ))
-            }
-          </ul>
+        </div>
+
+        <div className={styles.footer}>
+          <section>
+            <h2>Fund your Zap wallet</h2>
+            <p>Deposit to your wallet while your node is syncing so autopilot can start working magic for you</p>
+          </section>
+          <section>
+            <div className={styles.address}>
+              <span>2N9XC9e2wucq2Fzf8CqZGPU12Kq8H39vFxT</span>
+              <span className='hint--left' data-hint='Copy Address'>
+                <FaCopy />
+              </span>
+            </div>
+          </section>
         </div>
       </div>
     )
